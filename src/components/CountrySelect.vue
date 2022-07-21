5937a50b6838ab5932c070414665c49582b202ae
TirthakBM <tirthakbm123@gmail.com>
2022-07-21 16:02:03 +0530

    initial commit

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,32 @@
+<template>
+  <select
+    v-model="selected"
+    class="form-select mt-10 block w-full border p-3 rounded"
+    @change="onChange()"
+  >
+    <option value="0">Select Country</option>
+    <option v-for="country in countries" :value="country.ID">
+      {{ country.Country }}
+    </option>
+  </select>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+  name: 'CountrySelect',
+  props: ['countries'],
+  setup ({ countries }, { emit }) {
+    const selected = ref(0);
+    return {
+      selected,
+      onChange () {
+        const country = countries.find((item) => item.ID === selected.value);
+        emit('get-country', country);
+
+        console.log(country)
+      }
+    };
+  }
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,79 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main v-if="!loading">
+    <DataTitle :text="title" :dataDate="dataDate"/>
+    <DataBoxes :stats="stats"/>
+    <CountrySelect @get-country="getCountryData" :countries="countries"/>
+    <!-- <button
+      v-if="status.Country"
+      class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600"
+      @click="clearCountryData"
+    >
+      Clear Country
+    </button> -->
+  </main>
+
+  <main v-else class="flex flex-col align-center justify-center text-center">
+    <div class="text-gray-500 text-3xl mt-10 mb-6">
+      Fetching Data
+    </div>
+    <img :src="require('../assets/hourglass.gif')" alt="" class="w-24 m-auto" />
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+import DataTitleVue from '@/components/DataTitle.vue'
+import DataTitle from '@/components/DataTitle.vue'
+import DataBoxesVue from '@/components/DataBoxes.vue'
+import DataBoxes from '@/components/DataBoxes.vue'
+import CountrySelectVue from '@/components/CountrySelect.vue'
+import CountrySelect from '@/components/CountrySelect.vue'
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
-  }
+    DataTitleVue,
+    DataTitle,
+    DataBoxesVue,
+    DataBoxes,
+    CountrySelectVue,
+    CountrySelect
+},
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: '',
+      stats: {},
+      countries: [],
+      loadingImage: require('../assets/hourglass.gif')
+
+    }
+  },
+  methods:{
+     async fetchCovidData(){
+      const res = await fetch('https://api.covid19api.com/summary')
+      const data = await res.json()
+      return data
+    },
+    getCountryData(country){
+      this.stats = country
+      this.title = country.Country
+      
+    },
+    // async clearCountryData(){
+    //   this.loading = true
+    //   const data = await this.fetchCovidData()
+    //   this.title = "Global"
+    //   this.stats = data.Global
+    //   this.loading = false
+    // }
+  },
+  async created(){
+    const data = await this.fetchCovidData()
+    this.dataDate = data.Date
+    this.stats = data.Global
+    this.countries = data.Countries
+    this.loading = false
+  },
 }
 </script>
